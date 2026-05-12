ee88550b18b5e0899d8ed531c323a18f83642bc8
khushikohli09 <157889269+khushikohli09@users.noreply.github.com>
2026-05-12 10:14:19 +0530

    Delete BACKEND/routes  directory

--- BACKEND/routes /chatbotRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { chatbot } = require("../controllers/chatbotController");
-const  auth =require("../middleware/authMiddleware");
-// ❌ REMOVE auth for now (warna 401 aayega)
-router.post("/", auth,chatbot);
-
-module.exports = router;
